45b457b31312eebdb7107c82d9b6c106a812fb9c
Sachiththa Bandaranayake <sacume3@gmail.com>
2023-06-14 23:58:44 +0200

    :sparkles: Computer player middle

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-console.log("Sachiththa");
+const board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const players = ["player1", "player2"];
+const difficulty = ["easy", "hard"];
+const playerSymbol = {
+  player1: "X",
+  player2: "O",
+};
+let btns = document.querySelectorAll(".game-board-btn");
+let player = "player1";
 
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-players = ["player1", "player2"];
-difficulty = ["easy", "hard"];
+function test(btns, move, player) {
+  let element = btns[move];
+  element.textContent = playerSymbol[player];
+}
 
-function getMove(currentPlayer, board, difficulty) {
-  print("get move");
+function getMove(player, board, difficulty) {
+  if (player === "player2") {
+    return Promise.resolve(getBestMove(board, player));
+  }
+
+  return waitForBoardBtnClick();
 }
 
 function updateBoard(move, player, board) {
-  board[move] = "X";
-  print("update boad");
+  board[move] = playerSymbol[player];
 }
 
-function switchPlayer(currentPlayer, player) {
-  print("swithc player");
+function switchPlayer(players, player) {
+  currentPlayerIndex = players.indexOf(player);
+  nextPlayerIndex = (currentPlayerIndex + 1) % players.length;
+  return players[nextPlayerIndex];
+}
+
+function gameOver(board) {
+  return checkTie(board) || checkTie(board);
 }
 
 function checkWin(board) {
@@ -51,6 +69,26 @@ function checkWin(board) {
   return false;
 }
 
+function getBestMove(board, player) {
+  for (let i = 0; i < 9; i++) {
+    if (["X", "O"].indexOf(board[i]) < 0) {
+      const newBoard = structuredClone(board);
+      newBoard[i] = playerSymbol[(player.indexOf(player) + 1) % players.length];
+      if (checkWin(newBoard)) return i;
+    }
+  }
+
+  for (let i = 0; i < 9; i++) {
+    if (["X", "O"].indexOf(board[i]) < 0) {
+      const newBoard = structuredClone(board);
+      newBoard[i] = playerSymbol[player];
+      if (checkWin(newBoard)) return i;
+    }
+  }
+
+  return getComputerMove(board);
+}
+
 function checkTie(board) {
   const x = board.filter((cell) => cell === "X").length;
   const o = board.filter((cell) => cell === "O").length;
@@ -63,7 +101,7 @@ function checkTie(board) {
 function getComputerMove(board) {
   let emptyFields = [];
   for (let i = 0; i < 9; i++) {
-    if (board[i] !== "X" && board[i] !== "O") {
+    if (board[i] != "X" && board[i] != "O") {
       emptyFields.push(i);
     }
   }
@@ -75,4 +113,23 @@ function getComputerMove(board) {
   return emptyFields[Math.floor(Math.random() * emptyFields.length)];
 }
 
-move = getMove(currentPlayer, board, difficulty);
+function waitForBoardBtnClick() {
+  return new Promise((resolve, reject) => {
+    window.onBoardBtnClick = (element) => {
+      resolve(element.getAttribute("data-board-move"));
+    };
+  });
+}
+
+async function main() {
+  for (let i = 1; i < 10; i++) {
+    console.log(board);
+    let move = await getMove(player, board, "");
+    updateBoard(move, player, board);
+    test(btns, move, player);
+    player = switchPlayer(players, player);
+    console.log(board);
+  }
+}
+
+main();
